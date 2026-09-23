aa37c7d470be7aff69ac6ece082aecf9c3d499cc
sofiavasc <sofyvitoria015@gmail.com>
2026-09-23 14:30:30 -0300

    Update Cliente.kt

--- app/src/main/br.edu.ifpe..printflow/model/Cliente.kt
+++ app/src/main/br.edu.ifpe..printflow/model/Cliente.kt
@@ -11,13 +11,3 @@ data class Cliente(
     val telefone: String,
     val email: String
 )
-
-@Entity
-data class Servico(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
-    val nome: String,           
-    val descricao: String,
-    val precoUnitario: Double,  
-    val unidade: String = "un" 
-)
