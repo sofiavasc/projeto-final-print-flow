0bd6c6cac941d491b37ee840866953741e9140f2
sofiavasc <sofyvitoria015@gmail.com>
2026-09-23 13:20:41 -0300

    criação da Entidade Cliente.kt
    Responsável por essa entidade: Sofia Vitória

---
+++ app/src/main/br.edu.ifpe..printflow/model/Cliente.kt
@@ -0,0 +1,13 @@
+package br.edu.ifpe.printflow.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Cliente(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nome: String,
+    val telefone: String,
+    val email: String
+)
